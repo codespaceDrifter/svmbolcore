9f93aea59ad48049b8933294cd51c90f41ea4d1e
codespaceDrifter <codeCraftCorpo@gmail.com>
2025-09-22 03:07:20 -0700

    pypi problem need fix

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from svmbolcore import *
 
 vars = [
-    Variable ('a')
+    Variable ('a'),
     Variable ('b')
 ]
 eqs = [
-    Eq ( a + b = 5)
-    Eq ( a - b = 3)
-
+    parse_expr('a + b = 5'),
+    parse_expr('a - b = 3')
 ]
 
+sols = solve(eqs, vars)
+strs = sols_to_string_list(sols)
+assert strs[0] == "b : 1"
+assert strs[1] == "a : 4"
+print ("PARSE PRINT TESTS PASSED")
 
-sols = solve (eqs, vars)
-
-print (sols_to_string_list(sols))
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="svmbolcore",
-    version="0.1.16",
+    version="0.1.17",
     author="codespaceDrifter",  
     author_email="codespaceDrifter@gmail.com",  
     description="symbolic math lib that generates parallel CUDA",
